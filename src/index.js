85ad695a3a47e095335dfe177e5ed3aff119b5c8
Rakhymbek Sagyn <sagrakha@gmail.com>
2022-02-23 22:47:28 +0600

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    n = n.toString().split('').reverse();
+
+    if(n[0] === '0') {
+        n.splice(0, 1);
+    }else if(n.slice(-1) == '-') {
+        n.splice(-1, 1);
+    }
+
+    return n.join('');
 }
+
+
